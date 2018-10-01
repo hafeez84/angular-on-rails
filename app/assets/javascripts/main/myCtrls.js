e519b09debc6610aa5b2b405c9216197c83cc169
hafeez84 <h.qasemy@live.com>
2018-10-01 20:19:30 +0300

    fixed the bug on details controller. FIXME(pictures not loading)

--- app/assets/javascripts/main/myCtrls.js
+++ app/assets/javascripts/main/myCtrls.js
@@ -15,20 +15,21 @@ myCtrls.controller('Searchctrl',function ($scope, $http) {
 	});
 });
 
-myCtrls.controller('Detailsctrl', 
-	function($scope, $http, $routeParams){
+myCtrls.controller('Detailsctrl',
+	function($scope, $http, $stateParams){
 		$http.get('http://localhost:3000/users.json').then(function(response) {
 	    $scope.artists = response.data;
-	    $scope.whichItem = $routeParams.itemId;
+	    $scope.whichItem = $stateParams.itemId;
+	    console.log($stateParams.itemId);
 
-	    if ($routeParams.itemId > 0) {
-	      $scope.prevItem = Number($routeParams.itemId) - 1;
+	    if ($stateParams.itemId > 0) {
+	      $scope.prevItem = Number($stateParams.itemId) - 1;
 	    } else {
 	      $scope.prevItem = $scope.artists.length - 1;
 	    }
 
-	    if ($routeParams.itemId < $scope.artists.length-1) {
-	      $scope.nextItem = Number($routeParams.itemId) + 1;
+	    if ($stateParams.itemId < $scope.artists.length-1) {
+	      $scope.nextItem = Number($stateParams.itemId) + 1;
 	    } else {
 	      $scope.nextItem = 0;
 	    }
